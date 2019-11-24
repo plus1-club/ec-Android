1ece7ff33f81b17728d54d953ad3a3ab97e12376
lavsexpert <lavrov-sergey@yandex.ru>
2019-11-24 17:07:56 +0300

    Сделано новое меню (пока только на экране меню)
    - Новое меню такое же, как предыдущее отклонённое,
    только использует Activity вместо Fragment

--- app/src/main/java/club/plus1/ec_online/views/MenuActivity.java
+++ app/src/main/java/club/plus1/ec_online/views/MenuActivity.java
@@ -1,9 +1,18 @@
 package club.plus1.ec_online.views;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.databinding.DataBindingUtil;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
 
 import club.plus1.ec_online.R;
 import club.plus1.ec_online.databinding.MenuBinding;
@@ -12,14 +21,79 @@ import club.plus1.ec_online.viewmodels.MenuViewModel;
 public class MenuActivity extends AppCompatActivity {
 
     MenuViewModel viewModel;
-    MenuViewModel menuModel;
+    private DrawerLayout drawer;
+    private Toolbar toolbar;
+    private NavigationView navigation;
+    private ActionBarDrawerToggle drawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         MenuBinding binding = DataBindingUtil.setContentView(this, R.layout.menu);
         viewModel = new MenuViewModel(this);
-        menuModel = new MenuViewModel(this);
         binding.setViewModel(viewModel);
+
+        // Установить Toolbar для замены ActionBar'а.
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // Найти наш view drawer'а
+        drawer = findViewById(R.id.drawer);
+        drawerToggle = setupDrawerToggle();
+
+        // Привязать события DrawerLayout'а к ActionBarToggle
+        drawer.addDrawerListener(drawerToggle);
+        navigation = findViewById(R.id.navigator);
+        setupDrawerContent(navigation);
+
+        navigation.setItemIconTintList(null);
+    }
+
+    private void setupDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                        selectDrawerItem(menuItem);
+                        return true;
+                    }
+                });
+    }
+
+    public void selectDrawerItem(MenuItem menuItem) {
+        viewModel.onOptionsItemSelected(this, menuItem);
+        menuItem.setChecked(true);
+        setTitle(menuItem.getTitle());
+        drawer.closeDrawers();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            drawer.openDrawer(GravityCompat.START);
+            return true;
+        }
+        if (drawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        drawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(@NonNull Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        drawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    private ActionBarDrawerToggle setupDrawerToggle() {
+         return new ActionBarDrawerToggle(
+                 this, drawer, toolbar, R.string.drawer_open,  R.string.drawer_close);
     }
 }
