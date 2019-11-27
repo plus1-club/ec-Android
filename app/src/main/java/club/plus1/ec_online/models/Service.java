30f4b296ba6d1edaebebd83be641f546bf267bdc
lavsexpert <lavrov-sergey@yandex.ru>
2019-11-27 12:23:33 +0300

    Исправлен формат вывода сумм
    Теперь пробелы между тысячами и запятая отделяет рубли и копейки

--- app/src/main/java/club/plus1/ec_online/models/Service.java
+++ app/src/main/java/club/plus1/ec_online/models/Service.java
@@ -1,20 +1,14 @@
-package club.plus1.ec_online.viewadapters;
+package club.plus1.ec_online.models;
 
-import androidx.databinding.BindingConversion;
 import androidx.databinding.ObservableDouble;
 
 import java.text.DecimalFormat;
 import java.text.DecimalFormatSymbols;
 import java.util.Locale;
 
-import club.plus1.ec_online.App;
-import club.plus1.ec_online.R;
+public class Service {
 
-public class BindingConversions {
-
-    @BindingConversion
-    public static String convertObservableDoubleToString(ObservableDouble value) {
-        String template = App.getContext().getString(R.string.text_money);
+    public static String rub(ObservableDouble value, String template){
         DecimalFormat formatter=new DecimalFormat();
         DecimalFormatSymbols symbols=DecimalFormatSymbols.getInstance();
         symbols.setGroupingSeparator(' '); //явно задаем символ разделителя тысяч
@@ -22,5 +16,7 @@ public class BindingConversions {
         formatter.setDecimalFormatSymbols(symbols);
         String textValue = formatter.format(value.get());
         return String.format(Locale.getDefault(), template, textValue);
+
     }
+
 }
