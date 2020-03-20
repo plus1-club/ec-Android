86bc72ccc1609a934c88f6bb8d98d9aca3ed049d
lavsexpert <lavrov-sergey@yandex.ru>
2020-03-20 14:00:49 +0300

    Теперь можно редактировать текст в корзине с учётом кратности

--- app/src/main/java/ru/electric/ec/online/ui/basket/BasketItemViewModel.java
+++ app/src/main/java/ru/electric/ec/online/ui/basket/BasketItemViewModel.java
@@ -1,8 +1,12 @@
 package ru.electric.ec.online.ui.basket;
 
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.EditText;
 
 import androidx.databinding.ObservableBoolean;
 import androidx.databinding.ObservableDouble;
@@ -13,6 +17,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+import ru.electric.ec.online.R;
 import ru.electric.ec.online.common.Service;
 import ru.electric.ec.online.models.Basket;
 import ru.electric.ec.online.server.ServerRouter;
@@ -55,32 +60,48 @@ public class BasketItemViewModel {
         parent = BasketViewModel.getInstance();
     }
 
-    public void onTextChanged(Context context, CharSequence s, int start, int before, int charCount) {
-        int newCount;
-        try {
-            newCount = s.toString().isEmpty() ? 0 : Integer.parseInt(s.toString());
-        } catch (Exception e) {
-            newCount = 0;
-        }
-        if (newCount != count.get() && newCount != 0)
-        {
-            needUpdate.set(true);
-            if ((multiplicity.get() > 0) && (newCount % multiplicity.get() > 0)){
-                count.set(newCount + (multiplicity.get() - (newCount % multiplicity.get())));
-                //RouterView.openInfo(context, new Info(false, true,
-                // Service.getStr(R.string.text_multiplicity, multiplicity.get())));
-            } else {
-                count.set(newCount);
+    public void onTextClick(Context context){
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setTitle("Количество");
+
+        @SuppressLint("InflateParams")
+        View viewInflated = LayoutInflater.from(context).inflate(R.layout.edit_text, null, false);
+
+        final EditText input = viewInflated.findViewById(R.id.input);
+        input.setText(String.valueOf(count.get()));
+        builder.setView(viewInflated);
+
+        builder.setPositiveButton(android.R.string.ok, (dialog, which) -> {
+            dialog.dismiss();
+            String text = input.getText().toString();
+            int newCount;
+            try {
+                newCount = text.isEmpty() ? 0 : Integer.parseInt(text);
+            } catch (Exception e) {
+                newCount = 0;
             }
-            sum.set(count.get() * price.get());
-            Basket request = parent.basket.get(position.get());
-            request.needUpdate = needUpdate.get();
-            request.requestCount = count.get();
-            updateStatus();
-            parent.basket.set(position.get(), request);
-            Objects.requireNonNull(parent.adapter.get()).notifyItemChanged(position.get());
-        }
-     }
+            if (newCount != count.get() && newCount != 0)
+            {
+                needUpdate.set(true);
+                if ((multiplicity.get() > 0) && (newCount % multiplicity.get() > 0)){
+                    count.set(newCount + (multiplicity.get() - (newCount % multiplicity.get())));
+                    //RouterView.openInfo(context, new Info(false, true,
+                    // Service.getStr(R.string.text_multiplicity, multiplicity.get())));
+                } else {
+                    count.set(newCount);
+                }
+                sum.set(count.get() * price.get());
+                Basket request = parent.basket.get(position.get());
+                request.needUpdate = needUpdate.get();
+                request.requestCount = count.get();
+                parent.basket.set(position.get(), request);
+                Objects.requireNonNull(parent.adapter.get()).notifyItemChanged(position.get());
+                Objects.requireNonNull(parent.adapter.get()).notifyItemChanged(position.get());
+            }
+        });
+        builder.setNegativeButton(android.R.string.cancel, (dialog, which) -> dialog.cancel());
+        builder.show();
+    }
 
     public void onFlagClick(View view) {
         this.check.set(((CheckBox) view).isChecked());
